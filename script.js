89fe7d94691df34e8e6e8b44dde5c921d3a1055c
Joaquin Reynera <critizize.joaquin@gmail.com>
2023-03-27 17:29:45 +0800

    add history API

---
+++ script.js
@@ -0,0 +1,18 @@
+AOS.init();
+
+const resume = document.getElementById('resume');
+
+function openResume(){
+    resume.classList.remove('hidden');
+    window.history.pushState({ page: 'resume'}, 'resume', '#resume');
+}
+
+
+function closeResume(){
+    resume.classList.add('hidden');
+    window.history.go(-1);
+}
+
+window.addEventListener('popstate', function() {
+        closeResume();
+});
