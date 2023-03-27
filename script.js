071a1bce52220ce6cef265b8aeae41c319a76940
Joaquin Reynera <critizize.joaquin@gmail.com>
2023-03-28 03:58:00 +0800

    revert

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-AOS.init();
 
 const resume = document.getElementById('resume');
 
-function openResume(){
-    resume.classList.remove('hidden');
-    window.history.pushState({ page: 'resume'}, 'resume', '#resume');
-}
 
-
-function closeResume(){
-    resume.classList.add('hidden');
-    window.history.go(-1);
-}
-
-window.addEventListener('popstate', function(e) {
-    if (e.state){
-        closeResume();
-    }
-});
+AOS.init();
